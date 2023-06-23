2cf38784a4e9bb27ab89348ab09fa99bdf1899c0
seo <95337086+Yunseoyoung@users.noreply.github.com>
2023-06-23 14:04:20 +0900

    주소 수정 카멜방법

--- Wego/src/main/java/kr/co/main/review/ReviewController.java
+++ Wego/src/main/java/kr/co/main/review/ReviewController.java
@@ -13,43 +13,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/review")
 public class ReviewController {
-	@Autowired
-	ReviewService Service;
-	@GetMapping("/index.do")
-	public String index(Model model) {
-		List<ReviewVO> vo =  Service.test();
-		System.out.println("controller" + vo.toString());
-		model.addAttribute("vo", vo);
-		return "index";
-	}
+	
 	
 	
 	
 	//===
-	//상세 게시물 댓글 조회수
+	//상세 게시물 댓글 조회수 review_comment_write => view 리뷰 상세 보기
 	//===
-		@GetMapping("/review_comment_write.do")
+		@GetMapping("view.do")
 		public String view(Model model, ReviewVO vo) { //필요해서 param 다드러잇어 sword page 기본ㅏㄱㅄ으로 들어가잇고
 //			model.addAttribute("data", Service.view(vo));
-			return "review/review_comment_write";
+			return "review/view";
 		}
 		
 	//===
-	//후기 게시판 전체 목록 사진있는거
+	//후기 게시판 전체 목록 사진있는거 review => index 목록
 	//===
-		@GetMapping("/review.do")
+		@GetMapping("index.do")
 		public String review(Model model, ReviewVO vo) { //필요해서 param 다드러잇어 sword page 기본ㅏㄱㅄ으로 들어가잇고
 //			model.addAttribute("data", Service.view(vo));
-			return "review/review";
+			return "review/index";
 		}	
 		
 	//===
-	//수정
+	//수정 review_update => edit
 	//===
-		@GetMapping("/review_update.do")
+		@GetMapping("edit.do")
 		public String review_update(Model model, ReviewVO vo) { //필요해서 param 다드러잇어 sword page 기본ㅏㄱㅄ으로 들어가잇고
 //				model.addAttribute("data", Service.view(vo));
-			return "review/review_update";
+			return "review/edit";
 		}	
 		
 }
